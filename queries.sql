4f2aa89c122f88a1d54f8bbada9316779b0c5180
Kyle Porter <kyleporter@Kyles-Air.attlocal.net>
2020-01-22 14:32:50 -0500

    added model and sql

--- queries.sql
+++ queries.sql
@@ -2,8 +2,27 @@
 
 -- Display the ProductName and CategoryName for all products in the database. Shows 77 records.
 
+    SELECT product.productName, category.CategoryName 
+    FROM product
+    JOIN category ON product.CategoryId = category.id;
+
 -- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
+
+    SELECT [Order].id, shipper.CompanyName 
+    FROM [Order]
+    JOIN shipper ON [Order].ShipVia = shipper.id
+    WHERE OrderDate < '2012-08-09';
 
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
 
+    SELECT Product.ProductName, OrderDetail.Quantity
+    FROM OrderDetail
+    JOIN Product ON OrderDetail.ProductId = Product.id
+    WHERE Orderid = 10251;
+
 -- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records.
+
+    SELECT [Order].ID AS "OrderId", Customer.CompanyName AS "Company Name", Employee.LastName AS "Employee Last Name"
+    FROM [Order]
+    JOIN Customer ON [Order].CustomerId = Customer.id
+    JOIN Employee ON [Order].EmployeeId = Employee.id;
